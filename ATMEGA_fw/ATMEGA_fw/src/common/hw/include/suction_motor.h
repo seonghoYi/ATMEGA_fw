e2539417a6186901fff2cbfb1afc2001be7c96c3
seonghoYi <sh.Lee4781@gmail.com>
2021-06-24 14:57:08 +0900

    Add suction_motor driver

---
+++ ATMEGA_fw/ATMEGA_fw/src/common/hw/include/suction_motor.h
@@ -0,0 +1,16 @@
+﻿#ifndef SUCTION_MOTOR_H_
+#define SUCTION_MOTOR_H_
+
+#include "hw_def.h"
+
+#ifdef _USE_HW_SUCTION_MOTOR
+#define SUCTION_MOTOR_MAX_CH	HW_SUCTION_MOTOR_MAX_CH
+
+bool suctionMotorInit(void);
+bool suctionMotorIsInit(void);
+void suctionMotorRun(void);
+void suctionMotorStop(void);
+
+
+#endif
+#endif /* SUCTION_MOTOR_H_ */
--- ATMEGA_fw/ATMEGA_fw/src/hw/hw_def.h
+++ ATMEGA_fw/ATMEGA_fw/src/hw/hw_def.h
@@ -7,16 +7,19 @@
 #define _USE_HW_SYSTICK
 
 #define _USE_HW_UART
-#define		 HW_UART_MAX_CH			2
+#define		 HW_UART_MAX_CH				2
 
 #define _USE_HW_ROS
-#define		 HW_ROS_PACKET_BUF_MAX	512
+#define		 HW_ROS_PACKET_BUF_MAX		512
 
 #define _USE_HW_TIMER
-#define		 HW_TIMER8_MAX_CH		1
-#define		 HW_TIMER16_MAX_CH		2
+#define		 HW_TIMER8_MAX_CH			1
+#define		 HW_TIMER16_MAX_CH			2
 
 #define _USE_HW_SG90
-#define		 HW_SG90_MAX_CH			2
+#define		 HW_SG90_MAX_CH				2
+
+#define _USE_HW_SUCTION_MOTOR
+#define		 HW_SUCTION_MOTOR_MAX_CH	1
 
 #endif /* HW_DEF_H_ */
