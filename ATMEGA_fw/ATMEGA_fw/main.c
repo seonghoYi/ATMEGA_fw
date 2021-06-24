2e42bdfe42745a1dbeb5c64d2f1e395443b5cfe4
seonghoYi <sh.Lee4781@gmail.com>
2021-06-24 16:46:30 +0900

    Add bluetooth driver

--- ATMEGA_fw/ATMEGA_fw/main.c
+++ ATMEGA_fw/ATMEGA_fw/main.c
@@ -25,10 +25,8 @@
 /*----------------------------SYSTEM DEFINITIONS----------------------------*/
 #define SYSTICK_MODULE_ENABLED			//systick 활성화
 #define UART_MODULE_ENABLED				//uart 활성화
-#define ROS_MODULE_ENABLED				//ros 활성화
 #define TIMER_MODULE_ENABLED
-#define SERVO_MODULE_ENABLED
-#define SUCTION_MOTOR_MODULE_ENABLED
+
 
 /*----------------------------SYSTEM DEFINITIONS END----------------------------*/
 
@@ -53,6 +51,10 @@
 #define	HW_TIMER8_MAX_CH			1
 #define	HW_TIMER16_MAX_CH			2
 
+#define _USE_ROS_MODULE				//ros 사용
+#define _USE_BLUETOOTH_MODULE		
+#define _USE_SERVO_MODULE		
+#define _USE_SUCTION_MOTOR_MODULE
 
 
 /*----------------------------GLOBAL H/W MACRO DEFINITIONS END----------------------------*/
@@ -528,8 +530,419 @@ void UART_RxCpltCallback(uint8_t ch)
 #endif
 /*----------------------------UART END----------------------------*/
 
+/*----------------------------TIMER----------------------------*/
+#ifdef TIMER_MODULE_ENABLED
+
+#define TIMER8_MAX_CH		HW_TIMER8_MAX_CH
+#define TIMER16_MAX_CH		HW_TIMER16_MAX_CH
+
+#define TIM0	0
+#define TIM1	1
+#define TIM2	2
+#define TIM3	3
+
+#define CHANNEL_A	_DEF_CH_A
+#define CHANNEL_B	_DEF_CH_B
+#define CHANNEL_C	_DEF_CH_C
+
+typedef struct
+{
+	bool is_open;
+	uint8_t tccr;
+	uint8_t tcnt;
+	uint8_t ocr;
+} tim8_t;
+
+typedef struct
+{
+	bool is_open;
+	uint8_t tccra;
+	uint8_t tccrb;
+	uint8_t tccrc;
+	uint16_t tcnt;
+	uint16_t ocr[3];
+	uint16_t icr;
+} tim16_t;
+
+static tim8_t	tim8_tbl[TIMER8_MAX_CH];
+static tim16_t	tim16_tbl[TIMER16_MAX_CH];
+
+
+bool timerInit(void)
+{
+	bool ret = true;
+	
+	for (int i = 0; i < TIMER8_MAX_CH; i++)
+	{
+		tim8_tbl[i].is_open = false;
+		tim8_tbl[i].tccr = 0;
+		tim8_tbl[i].ocr = 0;
+		tim8_tbl[i].tcnt = 0;
+	}
+	for (int i = 0; i < TIMER16_MAX_CH; i++)
+	{
+		tim16_tbl[i].is_open = false;
+		tim16_tbl[i].tccra = 0;
+		tim16_tbl[i].tccrb = 0;
+		tim16_tbl[i].tccrc = 0;
+		tim16_tbl[i].tcnt = 0;
+		tim16_tbl[i].ocr[0] = 0;
+		tim16_tbl[i].ocr[1] = 0;
+		tim16_tbl[i].ocr[2] = 0;
+		tim16_tbl[i].icr = 0;
+	}
+	
+	return ret;
+}
+
+bool timerBegin(uint8_t ch_)
+{
+	bool ret = true;
+	
+	switch(ch_)
+	{
+		case TIM2:
+		SET_BIT(tim8_tbl[_DEF_TIM1].tccr, (1 << WGM20));
+		SET_BIT(tim8_tbl[_DEF_TIM1].tccr, (1 << CS20)); // Phase correct PWM f=31.372KHz (F_CPU = 16MHz) (in 8MHz case, set timer as same then will be f=15.686KHz)
+		
+		SET_BIT(tim8_tbl[_DEF_TIM1].tccr, (1 << COM21)); // port oc2 non inverted output
+		
+		tim8_tbl[_DEF_TIM1].is_open = true;
+		break;
+		case TIM1:
+		SET_BIT(tim16_tbl[_DEF_TIM2].tccra, (1 << WGM11) | (1 << WGM10));
+		SET_BIT(tim16_tbl[_DEF_TIM2].tccrb, (1 << WGM12) | (1 << CS11)); // 10-bit Fast PWM f=1.953KHz (F_CPU = 16MHz) (in 8MHz case, set timer as 9-bit Fast PWM prescaler 8 then will be same)
+		
+		SET_BIT(tim16_tbl[_DEF_TIM2].tccra, (1 << COM1A1) | (1 << COM1B1)); // port A, B non inverted output
+		
+		tim16_tbl[_DEF_TIM2].is_open = true;
+		break;
+		case TIM3:
+		SET_BIT(tim16_tbl[_DEF_TIM3].tccra, (1 << WGM31));
+		SET_BIT(tim16_tbl[_DEF_TIM3].tccrb, (1 << WGM33) | (1 << WGM32) | (1 << CS31) | (1 << CS30));
+		tim16_tbl[_DEF_TIM3].icr = 4999; // Fast PWM f=50Hz (in 8MHz case, set ICR3 as 2499)
+		
+		SET_BIT(tim16_tbl[_DEF_TIM3].tccra, (1 << COM3A1) | (1 << COM3B1)); // port A, B non inverted output
+		
+		tim16_tbl[_DEF_TIM3].is_open = true;
+		break;
+		#ifdef SYSTICK_MODULE_ENABLED
+		case TIM0:
+		
+		break;
+		#endif
+	}
+	
+	return ret;
+}
+
+bool timerIsOpen(uint8_t ch_)
+{
+	switch(ch_)
+	{
+		case TIM2: return tim8_tbl[_DEF_TIM1].is_open;
+		case TIM1: return tim16_tbl[_DEF_TIM2].is_open;
+		case TIM3: return tim16_tbl[_DEF_TIM3].is_open;
+		case TIM0: return tim8_tbl[_DEF_TIM4].is_open;
+		default:   return false;
+	}
+}
+
+
+bool timerStart(uint8_t ch_)
+{
+	switch(ch_)
+	{
+		case TIM2:
+		WRITE_REG(TCCR2, tim8_tbl[_DEF_TIM1].tccr);
+		WRITE_REG(TCNT2, tim8_tbl[_DEF_TIM1].tcnt);
+		WRITE_REG(OCR2, tim8_tbl[_DEF_TIM1].ocr);
+		break;
+		case TIM1:
+		WRITE_REG(TCCR1A, tim16_tbl[_DEF_TIM2].tccra);
+		WRITE_REG(TCCR1B, tim16_tbl[_DEF_TIM2].tccrb);
+		WRITE_REG(TCCR1C, tim16_tbl[_DEF_TIM2].tccrc);
+		WRITE_REG(OCR1A, tim16_tbl[_DEF_TIM2].ocr[CHANNEL_A]);
+		WRITE_REG(OCR1B, tim16_tbl[_DEF_TIM2].ocr[CHANNEL_B]);
+		WRITE_REG(OCR1C, tim16_tbl[_DEF_TIM2].ocr[CHANNEL_C]);
+		WRITE_REG(TCNT1, tim16_tbl[_DEF_TIM2].tcnt);
+		WRITE_REG(ICR1, tim16_tbl[_DEF_TIM2].icr);
+		break;
+		case TIM3:
+		WRITE_REG(TCCR3A, tim16_tbl[_DEF_TIM3].tccra);
+		WRITE_REG(TCCR3B, tim16_tbl[_DEF_TIM3].tccrb);
+		WRITE_REG(TCCR3C, tim16_tbl[_DEF_TIM3].tccrc);
+		WRITE_REG(OCR3A, tim16_tbl[_DEF_TIM3].ocr[CHANNEL_A]);
+		WRITE_REG(OCR3B, tim16_tbl[_DEF_TIM3].ocr[CHANNEL_B]);
+		WRITE_REG(OCR3C, tim16_tbl[_DEF_TIM3].ocr[CHANNEL_C]);
+		WRITE_REG(TCNT3, tim16_tbl[_DEF_TIM3].tcnt);
+		WRITE_REG(ICR3, tim16_tbl[_DEF_TIM3].icr);
+		break;
+		case TIM0:
+		WRITE_REG(TCCR0, tim8_tbl[_DEF_TIM4].tccr);
+		WRITE_REG(TCNT0, tim8_tbl[_DEF_TIM4].tcnt);
+		WRITE_REG(OCR0, tim8_tbl[_DEF_TIM4].ocr);
+		break;
+	}
+	return true;
+}
+
+bool timerStop(uint8_t ch_)
+{
+	switch(ch_)
+	{
+		case TIM2:
+		CLEAR_REG(TCCR2);
+		CLEAR_REG(TCNT2);
+		CLEAR_REG(OCR2);
+		break;
+		case TIM1:
+		CLEAR_REG(TCCR1A);
+		CLEAR_REG(TCCR1B);
+		CLEAR_REG(TCCR1C);
+		CLEAR_REG(OCR1A);
+		CLEAR_REG(OCR1B);
+		CLEAR_REG(OCR1C);
+		CLEAR_REG(TCNT1);
+		CLEAR_REG(ICR1);
+		break;
+		case TIM3:
+		CLEAR_REG(TCCR3A);
+		CLEAR_REG(TCCR3B);
+		CLEAR_REG(TCCR3C);
+		CLEAR_REG(OCR3A);
+		CLEAR_REG(OCR3B);
+		CLEAR_REG(OCR3C);
+		CLEAR_REG(TCNT3);
+		CLEAR_REG(ICR3);
+		break;
+		case TIM0:
+		CLEAR_REG(TCCR0);
+		CLEAR_REG(TCNT0);
+		CLEAR_REG(OCR0);
+		break;
+	}
+	return true;
+}
+
+
+bool timerSetTcnt(uint8_t ch_, uint16_t tcnt_)
+{
+	switch(ch_)
+	{
+		case TIM2:
+		tim8_tbl[_DEF_TIM1].tcnt = (uint8_t)tcnt_;
+		TCNT2 = tim8_tbl[_DEF_TIM1].tcnt;
+		break;
+		case TIM1:
+		tim16_tbl[_DEF_TIM2].tcnt = tcnt_;
+		TCNT1 = tim16_tbl[_DEF_TIM2].tcnt;
+		break;
+		case TIM3:
+		tim16_tbl[_DEF_TIM3].tcnt = tcnt_;
+		TCNT3 = tim16_tbl[_DEF_TIM3].tcnt;
+		break;
+		case TIM0:
+		tim8_tbl[_DEF_TIM4].tcnt = (uint8_t)tcnt_;
+		TCNT0 = tim8_tbl[_DEF_TIM4].tcnt;
+		default:
+		return false;
+	}
+	return true;
+}
+
+uint16_t timerGetTcnt(uint8_t ch_)
+{
+	switch(ch_)
+	{
+		case TIM2: return (uint16_t)TCNT2;
+		case TIM1: return TCNT1;
+		case TIM3: return TCNT3;
+		case TIM0: return (uint16_t)TCNT0;
+		default: return 0;
+	}
+}
+
+bool timer8SetOcr(uint8_t ch_, uint8_t ocr_)
+{
+	switch(ch_)
+	{
+		case TIM2:
+		tim8_tbl[_DEF_TIM1].ocr = ocr_;
+		OCR2 = tim8_tbl[_DEF_TIM1].ocr;
+		break;
+		case TIM0:
+		tim8_tbl[_DEF_TIM4].ocr = ocr_;
+		OCR0 = tim8_tbl[_DEF_TIM4].ocr;
+		break;
+		default:
+		return false;
+	}
+	return true;
+}
+
+uint8_t timer8GetOcr(uint8_t ch_)
+{
+	switch(ch_)
+	{
+		case TIM2: return tim8_tbl[_DEF_TIM1].ocr;
+		case TIM0: return tim8_tbl[_DEF_TIM4].ocr;
+		default: return 0;
+	}
+}
+
+bool timer16SetOcr(uint8_t ch_, uint16_t ocr_, uint8_t channel_)
+{
+	switch(ch_)
+	{
+		case TIM1:
+		if (channel_ == CHANNEL_A)
+		{
+			tim16_tbl[_DEF_TIM2].ocr[CHANNEL_A] = ocr_;
+			OCR1A = tim16_tbl[_DEF_TIM2].ocr[CHANNEL_A];
+		}
+		else if (channel_ == CHANNEL_B)
+		{
+			tim16_tbl[_DEF_TIM2].ocr[CHANNEL_B] = ocr_;
+			OCR1B = tim16_tbl[_DEF_TIM2].ocr[CHANNEL_B];
+		}
+		else if (channel_ == CHANNEL_C)
+		{
+			tim16_tbl[_DEF_TIM2].ocr[CHANNEL_C] = ocr_;
+			OCR1C = tim16_tbl[_DEF_TIM2].ocr[CHANNEL_C];
+		}
+		break;
+		case TIM3:
+		if (channel_ == CHANNEL_A)
+		{
+			tim16_tbl[_DEF_TIM3].ocr[CHANNEL_A] = ocr_;
+			OCR3A = tim16_tbl[_DEF_TIM3].ocr[CHANNEL_A];
+		}
+		else if (channel_ == CHANNEL_B)
+		{
+			tim16_tbl[_DEF_TIM3].ocr[CHANNEL_B] = ocr_;
+			OCR3B = tim16_tbl[_DEF_TIM3].ocr[CHANNEL_B];
+		}
+		else if (channel_ == CHANNEL_C)
+		{
+			tim16_tbl[_DEF_TIM3].ocr[CHANNEL_C] = ocr_;
+			OCR3C = tim16_tbl[_DEF_TIM3].ocr[CHANNEL_C];
+		}
+		break;
+		default:
+		return false;
+	}
+	return true;
+}
+
+uint16_t timer16GetOcr(uint8_t ch_, uint8_t channel_)
+{
+	switch(ch_)
+	{
+		case TIM1: return tim16_tbl[_DEF_TIM2].ocr[channel_];
+		case TIM3: return tim16_tbl[_DEF_TIM3].ocr[channel_];
+		default:   return 0;
+	}
+}
+
+bool timer16SetIcr(uint8_t ch_, uint16_t icr_)
+{
+	switch(ch_)
+	{
+		case TIM1:
+		tim16_tbl[_DEF_TIM2].icr = icr_;
+		ICR1 = tim16_tbl[_DEF_TIM2].icr;
+		break;
+		case TIM3:
+		tim16_tbl[_DEF_TIM3].icr = icr_;
+		ICR3 = tim16_tbl[_DEF_TIM3].icr;
+		break;
+		default:
+		return false;
+	}
+	
+	return true;
+}
+
+uint16_t timer16GetIcr(uint8_t ch_)
+{
+	switch(ch_)
+	{
+		case TIM1: return tim16_tbl[_DEF_TIM2].icr;
+		case TIM3: return tim16_tbl[_DEF_TIM3].icr;
+		default:   return 0;
+	}
+}
+
+
+#endif
+/*----------------------------TIMER END----------------------------*/
+
+/*----------------------------BLUETOOTH----------------------------*/
+#ifdef _USE_BLUETOOTH_MODULE
+#define BT_BUF_MAX	64
+
+typedef struct
+{
+	uint8_t name[BT_BUF_MAX];
+	uint8_t pswd[BT_BUF_MAX];
+	uint8_t	slave_addr_buf[BT_BUF_MAX];
+} bt_cfg_t;
+
+typedef struct
+{
+	uint16_t msg_len;
+	uint8_t *msgs;
+} bt_msg_t;
+
+typedef struct
+{
+	bool			is_open;
+	uint8_t			ch;
+	uint32_t		baud;
+	uint32_t		pre_time;
+	bt_msg_t		msg;
+	bt_cfg_t		bt_cfg;
+} bt_t;
+
+bool btOpen(bt_t *p_bt, uint8_t ch_, uint32_t baud_)
+{
+	bool ret = true;
+	p_bt->ch = ch_;
+	p_bt->baud = baud_;
+	p_bt->is_open = uartOpen(p_bt->ch);
+	return ret;
+}
+
+uint32_t btAvailable(bt_t *p_bt)
+{
+	uint32_t ret = uartAvailable(p_bt->ch);
+	return ret;
+}
+
+uint8_t btRead(bt_t *p_bt)
+{
+	uint8_t ret = uartRead(p_bt->ch);
+	return ret;
+}
+
+uint32_t btWrite(bt_t *p_bt)
+{
+	uint32_t ret;
+	ret = uartWrite(p_bt->ch, p_bt->msg.msgs, p_bt->msg.msg_len);
+	return ret;
+}
+
+bool btBegin(void)
+{
+	
+}
+
+#endif
+/*----------------------------BLUETOOTH END----------------------------*/
+
 /*----------------------------ROS----------------------------*/
-#ifdef ROS_MODULE_ENABLED
+#ifdef _USE_ROS_MODULE
 #define ROS_MAX_SERVICE		128
 #define ROS_PACKET_BUF_MAX	512
 
@@ -846,356 +1259,8 @@ void rosCallService(ros_t *p_ros, uint8_t service_ch_, uint8_t *params_)
 #endif
 /*----------------------------ROS END----------------------------*/
 
-/*----------------------------TIMER----------------------------*/
-#ifdef TIMER_MODULE_ENABLED
-
-#define TIMER8_MAX_CH		HW_TIMER8_MAX_CH
-#define TIMER16_MAX_CH		HW_TIMER16_MAX_CH
-
-#define TIM0	0
-#define TIM1	1
-#define TIM2	2
-#define TIM3	3
-
-#define CHANNEL_A	_DEF_CH_A
-#define CHANNEL_B	_DEF_CH_B
-#define CHANNEL_C	_DEF_CH_C
-
-typedef struct
-{
-	bool is_open;
-	uint8_t tccr;
-	uint8_t tcnt;
-	uint8_t ocr;
-} tim8_t;
-
-typedef struct
-{
-	bool is_open;
-	uint8_t tccra;
-	uint8_t tccrb;
-	uint8_t tccrc;
-	uint16_t tcnt;
-	uint16_t ocr[3];
-	uint16_t icr;
-} tim16_t;
-
-static tim8_t	tim8_tbl[TIMER8_MAX_CH];
-static tim16_t	tim16_tbl[TIMER16_MAX_CH];
-
-
-bool timerInit(void)
-{
-	bool ret = true;
-	
-	for (int i = 0; i < TIMER8_MAX_CH; i++)
-	{
-		tim8_tbl[i].is_open = false;
-		tim8_tbl[i].tccr = 0;
-		tim8_tbl[i].ocr = 0;
-		tim8_tbl[i].tcnt = 0;
-	}
-	for (int i = 0; i < TIMER16_MAX_CH; i++)
-	{
-		tim16_tbl[i].is_open = false;
-		tim16_tbl[i].tccra = 0;
-		tim16_tbl[i].tccrb = 0;
-		tim16_tbl[i].tccrc = 0;
-		tim16_tbl[i].tcnt = 0;
-		tim16_tbl[i].ocr[0] = 0;
-		tim16_tbl[i].ocr[1] = 0;
-		tim16_tbl[i].ocr[2] = 0;
-		tim16_tbl[i].icr = 0;
-	}
-	
-	return ret;
-}
-
-bool timerBegin(uint8_t ch_)
-{
-	bool ret = true;
-	
-	switch(ch_)
-	{
-		case TIM2:
-			SET_BIT(tim8_tbl[_DEF_TIM1].tccr, (1 << WGM20));
-			SET_BIT(tim8_tbl[_DEF_TIM1].tccr, (1 << CS20)); // Phase correct PWM f=31.372KHz (F_CPU = 16MHz) (in 8MHz case, set timer as same then will be f=15.686KHz)
-			
-			SET_BIT(tim8_tbl[_DEF_TIM1].tccr, (1 << COM21)); // port oc2 non inverted output
-			
-			tim8_tbl[_DEF_TIM1].is_open = true;
-		break;
-		case TIM1:
-			SET_BIT(tim16_tbl[_DEF_TIM2].tccra, (1 << WGM11) | (1 << WGM10)); 
-			SET_BIT(tim16_tbl[_DEF_TIM2].tccrb, (1 << WGM12) | (1 << CS11)); // 10-bit Fast PWM f=1.953KHz (F_CPU = 16MHz) (in 8MHz case, set timer as 9-bit Fast PWM prescaler 8 then will be same)
-			
-			SET_BIT(tim16_tbl[_DEF_TIM2].tccra, (1 << COM1A1) | (1 << COM1B1)); // port A, B non inverted output
-			
-			tim16_tbl[_DEF_TIM2].is_open = true;
-		break;
-		case TIM3:
-			SET_BIT(tim16_tbl[_DEF_TIM3].tccra, (1 << WGM31));
-			SET_BIT(tim16_tbl[_DEF_TIM3].tccrb, (1 << WGM33) | (1 << WGM32) | (1 << CS31) | (1 << CS30));
-			tim16_tbl[_DEF_TIM3].icr = 4999; // Fast PWM f=50Hz (in 8MHz case, set ICR3 as 2499)
-			
-			SET_BIT(tim16_tbl[_DEF_TIM3].tccra, (1 << COM3A1) | (1 << COM3B1)); // port A, B non inverted output
-			
-			tim16_tbl[_DEF_TIM3].is_open = true;
-		break;
-		#ifdef SYSTICK_MODULE_ENABLED
-		case TIM0:
-		
-		break;
-		#endif
-	}
-	
-	return ret;
-}
-
-bool timerIsOpen(uint8_t ch_)
-{
-	switch(ch_)
-	{
-		case TIM2: return tim8_tbl[_DEF_TIM1].is_open;
-		case TIM1: return tim16_tbl[_DEF_TIM2].is_open;
-		case TIM3: return tim16_tbl[_DEF_TIM3].is_open;
-		case TIM0: return tim8_tbl[_DEF_TIM4].is_open;
-		default:   return false;
-	}
-}
-
-
-bool timerStart(uint8_t ch_)
-{
-	switch(ch_)
-	{
-		case TIM2:
-			WRITE_REG(TCCR2, tim8_tbl[_DEF_TIM1].tccr);
-			WRITE_REG(TCNT2, tim8_tbl[_DEF_TIM1].tcnt);
-			WRITE_REG(OCR2, tim8_tbl[_DEF_TIM1].ocr);
-		break;
-		case TIM1:
-			WRITE_REG(TCCR1A, tim16_tbl[_DEF_TIM2].tccra);
-			WRITE_REG(TCCR1B, tim16_tbl[_DEF_TIM2].tccrb);
-			WRITE_REG(TCCR1C, tim16_tbl[_DEF_TIM2].tccrc);
-			WRITE_REG(OCR1A, tim16_tbl[_DEF_TIM2].ocr[CHANNEL_A]);
-			WRITE_REG(OCR1B, tim16_tbl[_DEF_TIM2].ocr[CHANNEL_B]);
-			WRITE_REG(OCR1C, tim16_tbl[_DEF_TIM2].ocr[CHANNEL_C]);
-			WRITE_REG(TCNT1, tim16_tbl[_DEF_TIM2].tcnt);
-			WRITE_REG(ICR1, tim16_tbl[_DEF_TIM2].icr);
-		break;
-		case TIM3:	
-			WRITE_REG(TCCR3A, tim16_tbl[_DEF_TIM3].tccra);
-			WRITE_REG(TCCR3B, tim16_tbl[_DEF_TIM3].tccrb);
-			WRITE_REG(TCCR3C, tim16_tbl[_DEF_TIM3].tccrc);
-			WRITE_REG(OCR3A, tim16_tbl[_DEF_TIM3].ocr[CHANNEL_A]);
-			WRITE_REG(OCR3B, tim16_tbl[_DEF_TIM3].ocr[CHANNEL_B]);
-			WRITE_REG(OCR3C, tim16_tbl[_DEF_TIM3].ocr[CHANNEL_C]);
-			WRITE_REG(TCNT3, tim16_tbl[_DEF_TIM3].tcnt);
-			WRITE_REG(ICR3, tim16_tbl[_DEF_TIM3].icr);
-		break;
-		case TIM0:
-			WRITE_REG(TCCR0, tim8_tbl[_DEF_TIM4].tccr);
-			WRITE_REG(TCNT0, tim8_tbl[_DEF_TIM4].tcnt);
-			WRITE_REG(OCR0, tim8_tbl[_DEF_TIM4].ocr);
-		break;
-	}
-	return true;
-}
-
-bool timerStop(uint8_t ch_)
-{
-	switch(ch_)
-	{
-		case TIM2:
-			CLEAR_REG(TCCR2);
-			CLEAR_REG(TCNT2);
-			CLEAR_REG(OCR2);
-		break;
-		case TIM1:
-			CLEAR_REG(TCCR1A);
-			CLEAR_REG(TCCR1B);
-			CLEAR_REG(TCCR1C);
-			CLEAR_REG(OCR1A);
-			CLEAR_REG(OCR1B);
-			CLEAR_REG(OCR1C);
-			CLEAR_REG(TCNT1);
-			CLEAR_REG(ICR1);
-		break;
-		case TIM3:
-			CLEAR_REG(TCCR3A);
-			CLEAR_REG(TCCR3B);
-			CLEAR_REG(TCCR3C);
-			CLEAR_REG(OCR3A);
-			CLEAR_REG(OCR3B);
-			CLEAR_REG(OCR3C);
-			CLEAR_REG(TCNT3);
-			CLEAR_REG(ICR3);
-		break;
-		case TIM0:
-			CLEAR_REG(TCCR0);
-			CLEAR_REG(TCNT0);
-			CLEAR_REG(OCR0);
-		break;
-	}
-	return true;
-}
-
-
-bool timerSetTcnt(uint8_t ch_, uint16_t tcnt_)
-{
-	switch(ch_)
-	{
-		case TIM2:
-			tim8_tbl[_DEF_TIM1].tcnt = (uint8_t)tcnt_;
-			TCNT2 = tim8_tbl[_DEF_TIM1].tcnt;
-		break;
-		case TIM1:
-			tim16_tbl[_DEF_TIM2].tcnt = tcnt_;
-			TCNT1 = tim16_tbl[_DEF_TIM2].tcnt;
-		break;
-		case TIM3:
-			tim16_tbl[_DEF_TIM3].tcnt = tcnt_;
-			TCNT3 = tim16_tbl[_DEF_TIM3].tcnt;
-		break;
-		case TIM0:
-			tim8_tbl[_DEF_TIM4].tcnt = (uint8_t)tcnt_;
-			TCNT0 = tim8_tbl[_DEF_TIM4].tcnt;
-		default: 
-		return false;
-	}
-	return true;
-}
-
-uint16_t timerGetTcnt(uint8_t ch_)
-{
-	switch(ch_)
-	{
-		case TIM2: return (uint16_t)TCNT2;
-		case TIM1: return TCNT1;
-		case TIM3: return TCNT3;
-		case TIM0: return (uint16_t)TCNT0;
-		default: return 0;
-	}
-}
-
-bool timer8SetOcr(uint8_t ch_, uint8_t ocr_)
-{
-	switch(ch_)
-	{
-		case TIM2:
-			tim8_tbl[_DEF_TIM1].ocr = ocr_;
-			OCR2 = tim8_tbl[_DEF_TIM1].ocr;
-		break;
-		case TIM0:
-			tim8_tbl[_DEF_TIM4].ocr = ocr_;
-			OCR0 = tim8_tbl[_DEF_TIM4].ocr;
-		break;
-		default: 
-		return false;
-	}
-	return true;
-}
-
-uint8_t timer8GetOcr(uint8_t ch_)
-{
-	switch(ch_)
-	{
-		case TIM2: return tim8_tbl[_DEF_TIM1].ocr;
-		case TIM0: return tim8_tbl[_DEF_TIM4].ocr;
-		default: return 0;
-	}
-}
-
-bool timer16SetOcr(uint8_t ch_, uint16_t ocr_, uint8_t channel_)
-{
-	switch(ch_)
-	{
-		case TIM1:
-			if (channel_ == CHANNEL_A)
-			{
-				tim16_tbl[_DEF_TIM2].ocr[CHANNEL_A] = ocr_;
-				OCR1A = tim16_tbl[_DEF_TIM2].ocr[CHANNEL_A];
-			}
-			else if (channel_ == CHANNEL_B)
-			{
-				tim16_tbl[_DEF_TIM2].ocr[CHANNEL_B] = ocr_;
-				OCR1B = tim16_tbl[_DEF_TIM2].ocr[CHANNEL_B];
-			}
-			else if (channel_ == CHANNEL_C)
-			{
-				tim16_tbl[_DEF_TIM2].ocr[CHANNEL_C] = ocr_;
-				OCR1C = tim16_tbl[_DEF_TIM2].ocr[CHANNEL_C];
-			}
-		break;
-		case TIM3:
-			if (channel_ == CHANNEL_A)
-			{
-				tim16_tbl[_DEF_TIM3].ocr[CHANNEL_A] = ocr_;
-				OCR3A = tim16_tbl[_DEF_TIM3].ocr[CHANNEL_A];
-			}
-			else if (channel_ == CHANNEL_B)
-			{
-				tim16_tbl[_DEF_TIM3].ocr[CHANNEL_B] = ocr_;
-				OCR3B = tim16_tbl[_DEF_TIM3].ocr[CHANNEL_B];
-			}
-			else if (channel_ == CHANNEL_C)
-			{
-				tim16_tbl[_DEF_TIM3].ocr[CHANNEL_C] = ocr_;
-				OCR3C = tim16_tbl[_DEF_TIM3].ocr[CHANNEL_C];
-			}
-		break;
-		default:
-		return false;
-	}
-	return true;
-}
-
-uint16_t timer16GetOcr(uint8_t ch_, uint8_t channel_)
-{
-	switch(ch_)
-	{
-		case TIM1: return tim16_tbl[_DEF_TIM2].ocr[channel_];
-		case TIM3: return tim16_tbl[_DEF_TIM3].ocr[channel_];
-		default:   return 0;
-	}
-}
-
-bool timer16SetIcr(uint8_t ch_, uint16_t icr_)
-{
-	switch(ch_)
-	{
-		case TIM1:
-			tim16_tbl[_DEF_TIM2].icr = icr_;
-			ICR1 = tim16_tbl[_DEF_TIM2].icr;
-		break;
-		case TIM3:
-			tim16_tbl[_DEF_TIM3].icr = icr_;
-			ICR3 = tim16_tbl[_DEF_TIM3].icr;
-		break;
-		default:
-		return false;
-	}
-	
-	return true;
-}
-
-uint16_t timer16GetIcr(uint8_t ch_)
-{
-	switch(ch_)
-	{
-		case TIM1: return tim16_tbl[_DEF_TIM2].icr;
-		case TIM3: return tim16_tbl[_DEF_TIM3].icr;
-		default:   return 0;
-	}
-}
-
-
-#endif
-/*----------------------------TIMER END----------------------------*/
-
 /*----------------------------SERVO----------------------------*/
-#ifdef SERVO_MODULE_ENABLED
+#ifdef _USE_SERVO_MODULE
 
 #define _DEF_SERVO_L	_DEF_CH_A
 #define _DEF_SERVO_R	_DEF_CH_B
@@ -1238,7 +1303,7 @@ void servoStop(void)
 
 
 /*----------------------------SUCTION MOTOR----------------------------*/
-#ifdef SUCTION_MOTOR_MODULE_ENABLED
+#ifdef _USE_SUCTION_MOTOR_MODULE
 
 static uint8_t suction_ch_tbl[] = {_DEF_TIM1, };
 static bool suction_is_init = false;
@@ -1257,7 +1322,7 @@ bool suctionMotorIsInit(void)
 
 void suctionMotorRun(void)
 {
-	timer8SetOcr(254);
+	timer8SetOcr(suction_ch_tbl[0], 254);
 	timerStart(suction_ch_tbl[0]);
 }
 
